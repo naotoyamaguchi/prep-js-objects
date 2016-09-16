2d07d08ffb311011c8be445e0277e18d45b3e56d
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-09-15 15:00:22 -1000

    finished 3

--- objects.js
+++ objects.js
@@ -50,6 +50,16 @@ var stockCar = {
         Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just the value at `name`, and just the value at `age`.
  */
 
+var plainPerson = {};
+
+function buildPerson(person, nameString, age){
+    person.name = nameString;
+    person.age = age;
+    return person;
+}
+    var completePerson = (buildPerson(plainPerson, "John Doe", 29));
+    console.log(completePerson);
+
 
 /*
     # Display values of objects that are inside an array
@@ -74,6 +84,68 @@ var stockCar = {
             ...
  */
 
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+function printProcessedOrders(orders){
+    for(var i = 0; i < orders.length; i++){
+        console.log("id: " + orders[i].id);
+        console.log("purchase date: " + orders[i].date);
+        console.log("purchase total: " + orders[i].total);
+    }
+}
+
+printProcessedOrders(arrayOfObjects);
 
 /*
     # Addition with an object
@@ -86,6 +158,19 @@ var stockCar = {
         Go ahead and create some more objects and pass them to this function. Have fun with it.
 */
 
+var sumObj = {
+	a : 5,
+	b : 10,
+	result : undefined
+};
+
+function objectAddition(test){
+	test.result = test.a + test.b;
+	return test;
+}
+
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
 
 /*
     # Print sum function and add as new key-value
